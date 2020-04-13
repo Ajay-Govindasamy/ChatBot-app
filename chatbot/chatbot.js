a5739bebf76dc6fa0c8db5698202cff804db2df8
Ajay-Govindasamy <63512607+Ajay-Govindasamy@users.noreply.github.com>
2020-04-13 10:21:08 +0530

    connect server side and dialogflow build2

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -10,7 +10,7 @@ const credentials = {
     private_key: config.googlePrivateKey
 };
 
-const sessionClient = new dialogflow.SessionsClient({projectID: projectID, credentials: credentials});
+const sessionClient = new dialogflow.SessionsClient({projectID, credentials});
 
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
